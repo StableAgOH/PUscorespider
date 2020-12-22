86c87afd87d1d130d8e4d9098b3fbdeb938f7716
StableAgOH <1305707442@qq.com>
2020-12-22 23:55:00 +0800

    美化单元格格式

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 '''常量'''
 import json
+import xlwt
+
 CONFIG = json.load(open("config.json", "r"))
 headers = CONFIG["headers"]
 school = CONFIG["school"]
@@ -12,3 +14,9 @@ if headers["Cookie"] is None or school is None:
 URL_PRE = "http://"+school+".pocketuni.net"
 URL_TP = URL_PRE+"/index.php?app=event&mod=School&act=rank&k={type}&p={page}"
 URL_USER = URL_PRE+"/index.php?app=home&mod=Account&act=index"
+
+alignment = xlwt.Alignment()
+alignment.horz = xlwt.Alignment.HORZ_CENTER
+alignment.vert = xlwt.Alignment.VERT_CENTER
+STYLE = xlwt.XFStyle()
+STYLE.alignment = alignment
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import bs4
 import xlwt
 import constants as ct
 
+
 def get_rq(url: str):
     '''从url获取一个get的requests'''
     return requests.get(url, headers=ct.headers)
@@ -24,21 +25,21 @@ def init_book():
     '''初始化工作簿'''
     book = xlwt.Workbook(encoding="UTF-8")
     sheet: xlwt.Worksheet = book.add_sheet("PUscore")
-    sheet.col(2).width = 100
-    sheet.write(0, 0, label="排名")
-    sheet.write(0, 1, label="姓名")
-    sheet.write(0, 2, label="学号")
-    sheet.write(0, 3, label="分数")
+    sheet.col(2).width = 2857
+    sheet.write(0, 0, label="排名", style=ct.STYLE)
+    sheet.write(0, 1, label="姓名", style=ct.STYLE)
+    sheet.write(0, 2, label="学号", style=ct.STYLE)
+    sheet.write(0, 3, label="分数", style=ct.STYLE)
     return book, sheet
 
 
 def write_data(sheet: xlwt.Worksheet, elements: list):
     '''向sheet中写入elements中的数据'''
     rank = int(elements[0].string)
-    sheet.write(rank, 0, label=rank)
-    sheet.write(rank, 1, label=elements[1].string)
-    sheet.write(rank, 2, label=elements[2].string)
-    sheet.write(rank, 3, label=float(elements[3].string))
+    sheet.write(rank, 0, label=rank, style=ct.STYLE)
+    sheet.write(rank, 1, label=elements[1].string, style=ct.STYLE)
+    sheet.write(rank, 2, label=elements[2].string, style=ct.STYLE)
+    sheet.write(rank, 3, label=float(elements[3].string), style=ct.STYLE)
 
 
 def get_rank_and_pages(type_: int):
