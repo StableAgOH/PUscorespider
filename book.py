3740436db81f0a5e0cf16a14ad3e64b8e934f856
AgOH <stagoh@qq.com>
2022-03-16 14:33:22 +0800

    Refactor

--- book.py
+++ book.py
@@ -1,10 +1,8 @@
-import os
-from typing import List
+import datetime
+from pathlib import Path
 
 import xlrd
 import xlwt
-
-from constants import BOOK_FOLDER, TODAY, BOOK_PATH, TYPES
 
 al = xlwt.Alignment()
 al.horz = xlwt.Alignment.HORZ_CENTER
@@ -12,38 +10,40 @@ al.vert = xlwt.Alignment.VERT_CENTER
 STYLE = xlwt.XFStyle()
 STYLE.alignment = al
 
+TODAY = datetime.date.today()
+
 
 class MyBook:
     def __init__(self):
+        self.folder = Path("books")
+        self.path = self.folder / Path(f"{TODAY.strftime('%Y%m%d')}.xls")
+
         self.book = xlwt.Workbook(encoding="UTF-8")
-        self.sheet: xlwt.Worksheet = self.book.add_sheet("PUscore")
+        self.sheet = self.book.add_sheet("PUscore")
         self.sheet.col(2).width = 2857
         self.write(1, ("排名", "姓名", "学号", "分数"))
         self.scores = []
 
-    def save(self):
-        if not os.path.exists(BOOK_FOLDER):
-            os.makedirs(os.getcwd() + '\\' + BOOK_FOLDER)
-        self.book.save(BOOK_PATH)
+    def exists(self):
+        return self.path.exists()
 
-    def write(self, row, data: tuple):
+    def save(self):
+        self.folder.mkdir(exist_ok=True)
+        self.book.save(self.path)
+
+    def write(self, row: int, data: tuple):
         for col in range(4):
             self.sheet.write(row, col, data[col], STYLE)
 
-    def write_data(self, data: List[List[int]]):
+    def write_data(self, data: list[list[int]]):
         for stu in data:
             ts = (int(stu[0]), str(stu[1]), str(stu[2]), float(stu[3]))  # rank, name, id, score
             self.write(int(stu[0]) + 1, ts)
             self.scores.append(float(stu[3]))
 
     def load_data(self):
-        book = xlrd.open_workbook(BOOK_PATH)
-        sheet = book.sheet_by_index(0)
+        sheet = xlrd.open_workbook(self.path).sheet_by_index(0)
         self.scores = sheet.col_values(3, 2)
 
-    def write_title(self, type_):
-        self.sheet.write_merge(
-            0, 0, 0, 3, TODAY.strftime("%Y/%m/%d") + "  " + TYPES[type_], STYLE)
-
-
-workbook = MyBook()
+    def write_title(self, tp: str):
+        self.sheet.write_merge(0, 0, 0, 3, f"{TODAY.strftime('%Y/%m/%d')} {tp}", STYLE)
--- pu.py
+++ pu.py
@@ -1,24 +1,86 @@
-import tqdm
+import json
+import logging
+import re
+from pathlib import Path
 
-from utils import *
+import requests
+import seaborn as sns
+import tqdm
+from bs4 import BeautifulSoup
+from matplotlib import pyplot as plt
+from requests.adapters import HTTPAdapter
+
+from book import MyBook
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+
+path_config = Path("config.json")
+if path_config.exists():
+    CONFIG = json.load(open(path_config, "r"))
+else:
+    CONFIG = {
+        "cookie": input("请输入Cookie："),
+        "school": input("请输入学校：")
+    }
+    json.dump(CONFIG, open(path_config, "w"), sort_keys=True, indent=4)
+
+ENDPOINT = f"https://{CONFIG['school']}.pocketuni.net/index.php?app=event&mod=School&act=rank"
+
+HEADERS = {
+    "Accept": "text/html",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/87.0.4280.88 Safari/537.36",
+    "Connection": "close",
+    "Cookie": CONFIG['cookie']
+}
+
+TYPES = ("月度排名", "学期排名", "学年排名")
+
+
+def get_bs(url: str):
+    ses = requests.session()
+    ses.mount("https://", requests.adapters.HTTPAdapter(max_retries=3))
+    return BeautifulSoup(ses.get(url, headers=HEADERS, timeout=30).content, "lxml")
+
+
+def input_yn(info: str):
+    opt = input(info).upper()
+    while opt not in ('Y', 'N'):
+        logger.error("选项错误")
+        opt = input(info).upper()
+    return opt
+
 
 if __name__ == "__main__":
-    if input_continue():
+    workbook = MyBook()
+    if not workbook.exists() or input_yn("数据已存在，是否继续获取(Y/N)"):
         for i in range(1, 4):
-            print(str(i)+"."+TYPES[i])
-        tp = input_type()
-        url_pre = URL_TP.format(type=tp)
-        rank, pagecnt = get_rank_and_pages(tp)
-        workbook.write_title(tp)
-        if rank != -1:
-            print(INFO_RANK % rank)
-        else:
-            output_red(ERR_NRK)
-        pages = input_pages(pagecnt)
-        tr = tqdm.trange(1, pages+1, ascii=True)
-        tr.set_description("Progress")
-        for page in tr:
-            table = get_bs_instance(add_page(url_pre, page)).table
+            print(f"{i}. {TYPES[i - 1]}")
+
+        tp = input("要爬取哪个类型的排名(1~3)：")
+        while not tp.isdigit() or not 1 <= int(tp) <= 3:
+            logger.error("类型错误")
+            tp = input("要爬取哪个类型的排名(1~3)：")
+        tp = int(tp) - 1
+
+        workbook.write_title(TYPES[tp])
+        first_page = get_bs(f"{ENDPOINT}&k={tp}&p=1")
+        try:
+            rank = int(re.match(r"\d+", first_page.find(class_="myrank").string).group())
+            logger.info(f"当前排名为：{rank}")
+        except AttributeError:
+            logger.warning("无法获取当前排名")
+
+        pagecnt = int(list(first_page.find(class_="page plist").children)[6].string[2:])
+        pages = input(f"共有{pagecnt}页数据，要爬取几页数据(每页10个同学，0为爬取所有页)：")
+        while not pages.isdigit() or not 0 <= int(pages) <= pagecnt:
+            logger.error("页数错误")
+            pages = input(f"共有{pagecnt}页数据，要爬取几页数据(每页10个同学，0为爬取所有页)：")
+        pages = pagecnt if pages == '0' else int(pages)
+
+        for page in tqdm.trange(1, pages + 1, ascii=True):
+            table = get_bs(f"{ENDPOINT}&k={tp}&p={page}").table
             data_page = [
                 [
                     table.contents[stu].contents[attr].string
@@ -30,6 +92,11 @@ if __name__ == "__main__":
         workbook.save()
     else:
         workbook.load_data()
-    if input_yn(QST_DAN) == 'Y':
-        show()
-    print(INFO_DONE)
+
+    if input_yn("是否进行数据分析(Y/N)") == 'Y':
+        sns.histplot(workbook.scores, binwidth=4, kde=True)
+        plt.title(f"{CONFIG['school']} PU Score")
+        plt.xlabel("Score")
+        plt.ylabel("Number of students")
+        plt.show()
+    logger.info("任务完成啦！ヽ(￣▽￣)ﾉ")
